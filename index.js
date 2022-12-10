7bb881fa7cf4316e05c1b2a5f8e7595ecda939cd
Jakub Kares (private) <jakub.kares@gmail.com>
2022-12-10 23:37:00 +0100

    Creating shapes and choosing right context menu

--- index.js
+++ index.js
@@ -2,9 +2,51 @@ class Shape {}
 class Rectangle {}
 class Circle {}
 
-class ShapeView {}
-class RectangleView {}
-class CircleView {}
+class ShapeView {
+  constructor(model) {
+    this.model = model;
+    this.el = document.createElement("div");
+    this.el.className = "position-absolute bg-warning border border-1 rounded";
+  }
+
+  render() {}
+
+  setElPosition() {
+    this.el.style.top = this.position.top;
+    this.el.style.left = this.position.left;
+  }
+}
+class RectangleView extends ShapeView {
+  constructor(model) {
+    super(model);
+    this.width = this.model.width;
+    this.height = this.model.height;
+    this.position = this.model.position;
+  }
+  render() {
+    this.el.style.height = this.height;
+    this.el.style.width = this.width;
+    this.setElPosition();
+    const container = document.getElementById("app-container"); // TODO
+    container.appendChild(this.el);
+  }
+}
+class CircleView extends ShapeView {
+  constructor(model) {
+    super(model);
+    this.radius = this.model.radius;
+    this.position = this.model.position;
+  }
+
+  render() {
+    this.el.style.height = this.radius;
+    this.el.style.width = this.radius;
+    this.el.classList.add("rounded-circle");
+    this.setElPosition();
+    const container = document.getElementById("app-container"); // TODO
+    container.appendChild(this.el);
+  }
+}
 class ContextMenuView {}
 
 class AppCollection {
@@ -13,19 +55,24 @@ class AppCollection {
 }
 class CollectionView {
   constructor() {
-    this.view = document.createElement("div");
+    this.el = document.createElement("div");
     this.collection = this.createCollection(); // this will save all shape models
 
-    this.view.className = "container px-1 bg-light border border-1 rounded";
-    this.view.style.height = "500px";
-    this.view.addEventListener("contextmenu", this.handleContextMenuClick);
+    this.el.className =
+      "container position-relative px-1 bg-light border border-1 rounded";
+    this.el.id = "app-container";
+    this.el.style.height = "500px";
+    this.el.addEventListener(
+      "contextmenu",
+      this.handleContextMenuClick.bind(this)
+    );
   }
 
   render(id) {
     // this should render main view
     // what about views for all of the shapes in collection?
     const container = document.getElementById(id);
-    container.appendChild(this.view);
+    container.appendChild(this.el);
   }
 
   /* this will take care of context menu appearence 
@@ -33,7 +80,17 @@ class CollectionView {
   - or you click on a shape */
   handleContextMenuClick(e) {
     e.preventDefault();
-    console.log(e);
+    if (this.isClickedOnObject(e)) {
+      // show simple context menu
+      alert("Area");
+    } else {
+      //show some options to manipulate with object
+      alert("Object");
+    }
+  }
+
+  isClickedOnObject(e) {
+    return e.target == this.el;
   }
 
   createCollection() {
@@ -44,3 +101,15 @@ class CollectionView {
 
 const appView = new CollectionView();
 appView.render("app");
+
+const rect = new RectangleView({
+  width: "50px",
+  height: "50px",
+  position: { top: "70px", left: "10px" },
+});
+rect.render();
+const circ = new CircleView({
+  radius: "70px",
+  position: { top: "170px", left: "150px" },
+});
+circ.render();
